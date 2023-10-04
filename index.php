880338229032fc616453e20865af3205fb0e4c4b
khiemnk <khiemnk>
2023-10-04 20:55:13 +0700

    fix css

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/inner/favicon.png" />
     <link rel="stylesheet" type="text/css" href="calenplugin/css/daterangepicker.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" /> -->
     <link rel="stylesheet" type="text/css" href="assets/css/animate.css" />
     <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
